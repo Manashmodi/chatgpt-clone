79f97af2f6e7d529a5cedbfafabb370f36c88a5a
ManashModi <86552684+Manashmodi@users.noreply.github.com>
2023-04-19 20:26:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post("/", async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt:req.body.input,
-            temperature: 0,
+            temperature: 1.0,
             max_tokens: 4000,
             top_p: 1,
             frequency_penalty: 0.5,
